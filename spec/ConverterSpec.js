ca41efff0904179846c51eddaca8887ea305d870
Shaneil Clarke <shaneilske@yahoo.com>
2019-01-14 18:19:26 +0000

    added feature to convert celsius temperatures to fahrenheit

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -2,9 +2,13 @@ describe("Temperature Converter", function() {
 
   describe("Temperatures", function() {
     it("Fahrenheit to Celsius", function() {
-      var result = convertFahrenheitToCelsius(100);
-
+      let result = convertFahrenheitToCelsius(100);
       expect(result).toEqual('37.8');
+    });
+
+    it("Celsius to Fahrenheit", function() {
+      let solution = convertCelsiusToFahrenheit(60);
+      expect(solution).toBe('140');
     });
   });
 });
--- src/Converter.js
+++ src/Converter.js
@@ -1,5 +1,10 @@
 /** Unit Converter **/
 function convertFahrenheitToCelsius(temperature) {
   celsius = (temperature - 32)/1.8
-  return celsius.toFixed(1);
-}
+  return celsius.toFixed(1)
+};
+
+function convertCelsiusToFahrenheit(temperature) {
+  fahrenheit = (temperature * 9/5) + 32
+  return fahrenheit.toFixed(0)
+};
