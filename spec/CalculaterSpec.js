91c45a966b0439dea19fbb144cbec9108f0b5c14
Shaneil Clarke <shaneilske@yahoo.com>
2019-01-15 11:04:39 +0000

    added calculater feature to add two numbers

---
+++ spec/CalculaterSpec.js
@@ -0,0 +1,9 @@
+describe('Calculater', function(){
+
+  describe('adds two numbers', function(){
+    it('returns sum', function(){
+      expect(add(2,4)).toBe(6);
+    });
+  })
+
+});
---
+++ src/Calculater.js
@@ -0,0 +1,5 @@
+// Calculater
+function add(num1, num2) {
+  sum = num1 + num2;
+  return sum;
+};
